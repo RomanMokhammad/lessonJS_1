7e7f7e24937a43aa6aa9f9ffec762c142ad236f2
Roman <rm388687@gmail.com>
2024-02-22 00:52:34 +0200

    js1

---
+++ NANami.js
@@ -0,0 +1,15 @@
+let a = "ami";
+
+let b = 123;
+
+let c = ""
+
+let d = ""
+
+c = (b-a)
+
+d= (c+a)
+
+
+console.log (d)
+
---
+++ task2.js
@@ -0,0 +1,7 @@
+let aboutMe = {name:"Roman", nickname:"ninja", number: 380967518110, mail:"rm388687@gmail.com", cex:"man",hobby:"fishing" }
+let aboutMe1 = {name:"Vlad", nickname:"user1", number: 380968690457, mail:"gh344677@gmail.com", cex:"man",hobby:"hunting" }
+let aboutMe2 = {name:"David", nickname:"user2", number: 380965523412, mail:"di355677@gmail.com", cex:"man",hobby:"sport" }
+
+let aboutMeArray =[aboutMe, aboutMe1, aboutMe2]
+
+console.log (aboutMeArray)
